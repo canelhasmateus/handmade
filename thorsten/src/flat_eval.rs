9c3165873036ad514a2de8512e34f91ec6c8e243
Mateus Canelhas <mateus.canelhas@revolut.com>
2024-03-30 12:13:02 -0300

    Thorsten: Complete flat interpreter

--- thorsten/src/flat_eval.rs
+++ thorsten/src/flat_eval.rs
@@ -23,6 +23,13 @@ enum Object {
 }
 
 #[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone)]
+enum Return {
+    Value(Object),
+    Return(Object),
+    Error(String),
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone)]
 struct Function {
     parameters: Vec<String>,
     body: Vec<StatementId>,
@@ -42,24 +49,37 @@ fn eval(input: &str) -> Object {
 
     let vm = VM { table, input };
     let mut env = BTreeMap::new();
-    let mut result = Object::Null;
+    let mut result = Return::Value(Object::Null);
     for statement in statements {
         result = vm.eval_statement(&statement, &mut env);
     }
-    result
+    match result {
+        Return::Value(v) => v,
+        Return::Return(r) => r,
+        Return::Error(e) => Object::Text(e), // lol
+    }
 }
 
 impl VM<'_> {
-    fn eval_statement(&self, statement: &RawStatement, env: &mut Env) -> Object {
+    fn eval_statement(&self, statement: &RawStatement, env: &mut Env) -> Return {
         match statement.kind {
             RawStatementKind::ExprStmt { ref expr } => self.eval_expr(expr, env),
-            RawStatementKind::ReturnStmt { ref expr } => self.eval_expr(expr, env),
+            RawStatementKind::ReturnStmt { ref expr } => match self.eval_expr(expr, env) {
+                Return::Value(v) => Return::Return(v),
+                Return::Return(_) => unreachable!("return in return position"),
+                e @ Return::Error(_) => e,
+            },
             RawStatementKind::LetStmt { ref name, ref expr } => {
                 let RawExpression { ref range, .. } = self.table.get_expression(name);
                 let name = &self.input[range.start..range.end];
-                let value = self.eval_expr(expr, env);
-                env.insert(name.to_string(), value);
-                Object::Null
+                match self.eval_expr(expr, env) {
+                    Return::Return(_) => unreachable!("Return in let statement position"),
+                    e @ Return::Error(_) => e,
+                    Return::Value(v) => {
+                        env.insert(name.to_string(), v);
+                        Return::Value(Object::Null)
+                    }
+                }
             }
 
             RawStatementKind::EndStatement => todo!(),
@@ -67,7 +87,7 @@ impl VM<'_> {
         }
     }
 
-    fn eval_expr(&self, expr: &ExpressionId, env: &mut Env) -> Object {
+    fn eval_expr(&self, expr: &ExpressionId, env: &mut Env) -> Return {
         let RawExpression { ref range, kind } = self.table.get_expression(expr);
 
         match kind {
@@ -93,71 +113,93 @@ impl VM<'_> {
             RawExpressionKind::Call { ref function, arguments } => {
                 self.eval_call(function, arguments, env)
             }
-            RawExpressionKind::IndexExpression { .. } => todo!(),
+            RawExpressionKind::IndexExpression { ref left, ref idx } => {
+                self.eval_index(left, idx, env)
+            }
             RawExpressionKind::IllegalExpression => todo!(),
         }
     }
 
-    fn eval_integer(&self, range: &Range) -> Object {
-        self.input[range.start..range.end]
+    fn eval_integer(&self, range: &Range) -> Return {
+        let content = &self.input[range.start..range.end];
+        content
             .parse::<i64>()
-            .map(|i| Object::Integer(i))
-            .unwrap_or(Object::Integer(0))
+            .map(|i| Return::Value(Object::Integer(i)))
+            .unwrap_or_else(|_| Return::Error(format!("Cannot parse {} as integer", content)))
     }
 
-    fn eval_text(&self, range: &Range) -> Object {
+    fn eval_text(&self, range: &Range) -> Return {
         let unquoted = (range.start + 1)..(range.end - 1);
         let content = self.input[unquoted].to_string();
-        Object::Text(content)
+        Return::Value(Object::Text(content))
     }
 
-    fn eval_bool(&self, range: &Range) -> Object {
-        match &self.input[range.start..range.end] {
-            "true" => Object::Boolean(true),
-            "false" => Object::Boolean(false),
-            _ => unreachable!(),
-        }
+    fn eval_bool(&self, range: &Range) -> Return {
+        let content = &self.input[range.start..range.end];
+        content
+            .parse::<bool>()
+            .map(|b| Return::Value(Object::Boolean(b)))
+            .unwrap_or_else(|_| Return::Error(format!("Cannot parse {} as boolean", content)))
     }
 
-    fn eval_identifier(&self, range: &Range, env: &mut Env) -> Object {
-        env.get(&self.input[range.start..range.end])
-            .map(|a| a.clone())
-            .unwrap_or(Object::Null)
+    fn eval_identifier(&self, range: &Range, env: &mut Env) -> Return {
+        let key = &self.input[range.start..range.end];
+        let value = env.get(key).map(|a| a.clone()).unwrap_or(Object::Null);
+        Return::Value(value)
     }
 
-    fn eval_list(&self, values: &[ExpressionId], env: &mut Env) -> Object {
+    fn eval_list(&self, values: &[ExpressionId], env: &mut Env) -> Return {
         let mut result = vec![];
         for expr in values {
-            result.push(self.eval_expr(expr, env))
+            match self.eval_expr(expr, env) {
+                Return::Value(b) => result.push(b),
+                Return::Return(b) => unreachable!("Return expr inside a list?"),
+                e @ Return::Error(_) => return e,
+            }
         }
-        Object::List(result)
+        Return::Value(Object::List(result))
     }
 
-    fn eval_map(&self, values: &[(ExpressionId, ExpressionId)], env: &mut Env) -> Object {
-        let map = values
-            .iter()
-            .map(|(key, value)| (self.eval_expr(key, env), self.eval_expr(value, env)))
-            .collect::<BTreeMap<Object, Object>>();
+    fn eval_map(&self, values: &[(ExpressionId, ExpressionId)], env: &mut Env) -> Return {
+        let mut map = BTreeMap::<Object, Object>::new();
+        for (key, value) in values {
+            let key = match self.eval_expr(key, env) {
+                Return::Value(v) => v,
+                Return::Return(_) => unreachable!("Return in map key position?"),
+                e @ Return::Error(_) => return e,
+            };
 
-        Object::Map(map)
+            let value = match self.eval_expr(value, env) {
+                Return::Value(v) => v,
+                Return::Return(_) => unreachable!("Return in map value position?"),
+                e @ Return::Error(_) => return e,
+            };
+
+            map.insert(key, value);
+        }
+
+        Return::Value(Object::Map(map))
     }
 
-    fn eval_unary(&self, op: UnaryOp, expr: &ExpressionId, env: &mut Env) -> Object {
-        let value = self.eval_expr(expr, env);
+    fn eval_unary(&self, op: UnaryOp, expr: &ExpressionId, env: &mut Env) -> Return {
+        let value = match self.eval_expr(expr, env) {
+            Return::Value(v) => v,
+            e @ Return::Error(_) => return e,
+            Return::Return(r) => unreachable!("Return in unary position"),
+        };
 
-        match op {
+        let result = match op {
             OpNot => match value {
-                Object::Boolean(b) => match b {
-                    false => Object::Boolean(true),
-                    true => Object::Boolean(false),
-                },
+                Object::Boolean(b) => Object::Boolean(!b),
                 _ => unreachable!("Can't not non-boolean"),
             },
             OpNeg => match value {
                 Object::Integer(i) => Object::Integer(-i),
                 _ => unreachable!("Can't negate non-integer"),
             },
-        }
+        };
+
+        Return::Value(result)
     }
 
     fn eval_binary(
@@ -166,10 +208,20 @@ impl VM<'_> {
         left: &ExpressionId,
         right: &ExpressionId,
         env: &mut Env,
-    ) -> Object {
-        let left = self.eval_expr(left, env);
-        let right = self.eval_expr(right, env);
-        match (left, right) {
+    ) -> Return {
+        let left = match self.eval_expr(left, env) {
+            Return::Value(v) => v,
+            Return::Return(_) => unreachable!("Return in binary position"),
+            e @ Return::Error(_) => return e,
+        };
+
+        let right = match self.eval_expr(right, env) {
+            Return::Value(v) => v,
+            Return::Return(_) => unreachable!("Return in binary position"),
+            e @ Return::Error(_) => return e,
+        };
+
+        let result = match (left, right) {
             (Object::Integer(l), Object::Integer(r)) => match op {
                 BinaryOp::Plus => Object::Integer(l + r),
                 BinaryOp::Minus => Object::Integer(l - r),
@@ -183,26 +235,28 @@ impl VM<'_> {
 
             (Object::Text(l), Object::Text(r)) => match op {
                 BinaryOp::Plus => Object::Text(format!("{l}{r}")),
-                BinaryOp::Minus => todo!(),
-                BinaryOp::Times => todo!(),
-                BinaryOp::Div => todo!(),
                 BinaryOp::Greater => Object::Boolean(l > r),
                 BinaryOp::Lesser => Object::Boolean(l < r),
                 BinaryOp::Equals => Object::Boolean(l == r),
                 BinaryOp::Differs => Object::Boolean(l != r),
+                BinaryOp::Minus => return Return::Error("Cant subtract strings".to_owned()),
+                BinaryOp::Times => return Return::Error("Cant multiply strings".to_owned()),
+                BinaryOp::Div => return Return::Error("Cant divide strings".to_owned()),
             },
 
             (l, r) => match op {
                 BinaryOp::Equals => Object::Boolean(l == r),
                 BinaryOp::Differs => Object::Boolean(l != r),
-                BinaryOp::Plus => unreachable!(),
-                BinaryOp::Minus => unreachable!(),
-                BinaryOp::Times => unreachable!(),
-                BinaryOp::Div => unreachable!(),
-                BinaryOp::Greater => unreachable!(),
-                BinaryOp::Lesser => unreachable!(),
+                BinaryOp::Plus => return Return::Error("Cant add these things".to_owned()),
+                BinaryOp::Minus => return Return::Error("Cant subtract these things".to_owned()),
+                BinaryOp::Times => return Return::Error("Cant multiply these things".to_owned()),
+                BinaryOp::Div => return Return::Error("Cant divide these things".to_owned()),
+                BinaryOp::Greater => return Return::Error("Cant compare these things".to_owned()),
+                BinaryOp::Lesser => return Return::Error("Cant compare these things".to_owned()),
             },
-        }
+        };
+
+        Return::Value(result)
     }
 
     fn eval_conditional(
@@ -211,19 +265,27 @@ impl VM<'_> {
         positive: &[StatementId],
         negative: &[StatementId],
         env: &mut Env,
-    ) -> Object {
+    ) -> Return {
         let ok = match self.eval_expr(condition, env) {
-            Object::Boolean(b) => b,
-            _ => unreachable!("No truthy around here baby"),
+            Return::Return(_) => unreachable!("Return in if condition"),
+            e @ Return::Error(_) => return e,
+            Return::Value(v) => match v {
+                Object::Boolean(b) => b,
+                _ => unreachable!("No truthy around here baby"),
+            },
         };
 
         let statements = if ok { positive } else { negative };
-        statements
-            .iter()
-            .map(|id| self.table.get_statement(id))
-            .fold(Object::Null, |_, statement| {
-                self.eval_statement(statement, env)
-            })
+        let mut result = Return::Value(Object::Null);
+        for statement in statements {
+            let statement = self.table.get_statement(statement);
+            match self.eval_statement(statement, env) {
+                v @ Return::Value(_) => result = v,
+                r @ Return::Return(_) => return r,
+                e @ Return::Error(_) => return e,
+            };
+        }
+        result
     }
 
     fn eval_function(
@@ -232,7 +294,7 @@ impl VM<'_> {
         parameters: &[ExpressionId],
         body: &[StatementId],
         env: &mut Env,
-    ) -> Object {
+    ) -> Return {
         let name = &self.input[range.start..range.end];
         let func = Function {
             body: body.to_vec(),
@@ -246,7 +308,7 @@ impl VM<'_> {
 
         let result = Object::Function(func);
         env.insert(name.to_string(), result.clone());
-        result
+        Return::Value(result)
     }
 
     fn eval_call(
@@ -254,18 +316,22 @@ impl VM<'_> {
         function: &ExpressionId,
         arguments: &[ExpressionId],
         outer_env: &mut Env,
-    ) -> Object {
-        let bindings = arguments
-            .iter()
-            .map(|el| self.eval_expr(el, outer_env))
-            .collect::<Vec<_>>();
+    ) -> Return {
+        let mut bindings = vec![];
+        for arg in arguments {
+            match self.eval_expr(arg, outer_env) {
+                Return::Value(v) => bindings.push(v),
+                Return::Return(_) => unreachable!("Return in call position"),
+                e @ Return::Error(_) => return e,
+            }
+        }
 
         let Function { parameters, body, env } = {
             let RawExpression { ref range, .. } = self.table.get_expression(function);
             let name = &self.input[range.start..range.end];
             let value = outer_env.get(name);
             match value {
-                None => return Object::Null,
+                None => return Return::Error(format!("Function not found: {}", name)),
                 Some(o) => match o {
                     Object::Function(f) => f.clone(),
                     _ => unreachable!(),
@@ -273,18 +339,61 @@ impl VM<'_> {
             }
         };
 
-        let mut env = env.clone();
-        for (arg, param) in zip(parameters, bindings) {
-            env.insert(arg, param);
-        }
+        let mut env = {
+            let mut e = outer_env.clone();
+            e.extend(env);
+            for (arg, param) in zip(parameters, bindings) {
+                e.insert(arg, param);
+            }
+            e
+        };
 
         let mut result = Object::Null;
         for id in body {
             let statement = self.table.get_statement(&id);
-            result = self.eval_statement(&statement, &mut env)
+            match self.eval_statement(&statement, &mut env) {
+                Return::Value(v) => result = v,
+                Return::Return(r) => return Return::Value(r),
+                e @ Return::Error(_) => return e,
+            }
         }
 
-        result
+        Return::Value(result)
+    }
+
+    fn eval_index(&self, left: &ExpressionId, idx: &ExpressionId, env: &mut Env) -> Return {
+        let left = match self.eval_expr(left, env) {
+            Return::Value(v) => v,
+            Return::Return(_) => unreachable!("Return in indexing position"),
+            e @ Return::Error(_) => return e,
+        };
+
+        let idx = match self.eval_expr(idx, env) {
+            Return::Value(v) => v,
+            Return::Return(_) => unreachable!("Return in indexand position"),
+            e @ Return::Error(_) => return e,
+        };
+
+        let result = match (left, idx) {
+            (Object::List(values), Object::Integer(i)) => match values.get(i as usize) {
+                Some(v) => v.clone(),
+                None => unreachable!(),
+            },
+
+            (Object::Text(l), Object::Integer(r)) => {
+                let val = l.as_bytes()[r as usize] as char;
+                Object::Text(String::from(val))
+            }
+
+            (Object::Map(values), o) => values
+                .get(&o)
+                .map(|o| o.clone())
+                .unwrap_or_else(|| Object::Null),
+
+            _ => unreachable!(),
+        };
+
+        Return::Value(result)
     }
 }
 
@@ -342,6 +451,7 @@ mod tests {
         assert_eq!(eval("2 < 1"), Object::Boolean(false));
         assert_eq!(eval("\"a\" == \"a\""), Object::Boolean(true));
         assert_eq!(eval("\"a\" == \"b\""), Object::Boolean(false));
+        assert_eq!(eval("\"a\" + \"b\""), Object::Text("ab".to_string()));
     }
 
     #[test]
@@ -400,5 +510,47 @@ mod tests {
         double(4)
         "#;
         assert_eq!(eval(input), Object::Integer(8));
+    }
+
+    #[test]
+    fn index() {
+        let input = r#"
+        let map = { true : "a"}
+        let text= "abacate"
+        let list = [true, false, "c"] 
+        map[true] + text[1] + list[2]
+        "#;
+        assert_eq!(eval(input), Object::Text("abc".to_string()));
+    }
+
+    #[test]
+    fn early_returns() {
+        let input = r#"
+        let conditional = fn() {
+            if ( 1 < 2 ) {  
+                return true
+            }
+            return false
+        }
+        conditional()
+        "#;
+        assert_eq!(eval(input), Object::Boolean(true));
+    }
+
+    #[test]
+    fn fibonacci() {
+        let input = r#"
+        let fibonacci = fn(x) {
+            if (x < 1) {  
+                return 0
+            }
+            if (x<2) {
+            return 1 }
+
+            return fibonacci(x-1) + fibonacci(x-2)
+        }
+        fibonacci(10)
+        "#;
+        assert_eq!(eval(input), Object::Integer(55));
     }
 }
